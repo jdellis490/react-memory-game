b3832fee509dc16097298c3de38f111079e51349
Justin Ellis <jdellis490@gmail.com>
2022-11-14 21:15:45 -0700

    Added timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
 
 export const Timer = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        let timer = null;
+        if(isActive){
+            timer = setInterval(() => {
+                setSeconds((seconds) => seconds + 1);
+            }, 1000);
+        }
+        return () => {
+            clearInterval(timer);
+        };
+    });
+
   return (
-    <div className='timekeeper'>Timer
-    
+    <div className="timekeeper">
+      Timer: {seconds}
+      <br />
+        <button className="btn btn-block" onClick={()=>{setIsActive(true)}}>Start</button>
+        <button className="btn-reset btn-block">Reset</button>      
     </div>
-  )
-}
+  );
+};
